c3ac84f250e90cb9d967c5b09063c72bdaa6b021
Luc Schulz <luc@lucschulz.com>
2024-09-30 19:07:16 -0400

    Fixed formatting

--- functions.js
+++ functions.js
@@ -1,7 +1,6 @@
 function fieldsToArray() {
-    var sqlValues = document.getElementById("sqlValues").value;
-    var fields = sqlValues.split("\n");
-
+    let sqlValues = document.getElementById("sqlValues").value;
+    let fields = sqlValues.split("\n");
     for (let i = 0; i < fields.length; i++) {
         fields[i] = fields[i].trim();        
     }
@@ -9,16 +8,15 @@ function fieldsToArray() {
 }
 
 function arrayToSqlStatement() {
-    var array = fieldsToArray();
-    var formattedSql = "";
+    let array = fieldsToArray();
+    let formattedSql = "";
     for (let i = 0; i < array.length; i++) {
-        var field = array[i];
+        let field = array[i];
         if (field.length > 0) {
             field = "ApplicationNumber = '" + field + "' OR ";
             formattedSql += field;
         }        
     }
-
     formattedSql = formattedSql.substring(0, formattedSql.length - 4) + ";";
     formattedSql = "SELECT * FROM [TABLE] WHERE " + formattedSql;
     document.getElementById("formattedSql").value = formattedSql;
